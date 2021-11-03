806440eb9dab7181dd639db9d9c2e245677324a6
moostofa <mmdrahinz96@gmail.com>
2021-11-03 14:23:45 +1100

    added "seller" column in listings & changed floatfield -> decimalfield

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import CharField, DateTimeField, FloatField, IntegerField, TextField
+from django.db.models.fields import CharField, DateTimeField, DecimalField, IntegerField, TextField
 from django.db.models.fields.related import ForeignKey
 
 #website users
@@ -11,16 +11,18 @@ class User(AbstractUser):
 
 class Listing(models.Model):
     title = CharField(max_length=20)
-    price = FloatField()
+    price = DecimalField(max_digits=9, decimal_places=2)
+    seller = CharField(max_length=20)
     created = DateTimeField(editable=False, auto_now_add=True)
     description = TextField()
-    category = CharField(max_length=10, blank=True, null=True)
+    category = CharField(max_length=20, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"""
         title = {self.title},
         price = ${self.price},
-        time created (UTC) = {self.created},
+        seller = {self.seller}
+        time created = {self.created},
         description = {self.description},
         category = {self.category}
         """
@@ -31,7 +33,7 @@ class Bid(models.Model):
         Listing,
         on_delete=models.CASCADE
     )
-    bid = FloatField()
+    bid = DecimalField(max_digits=9, decimal_places=2)
     bid_count = IntegerField()
 
 
