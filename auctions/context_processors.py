a80124eefac9ee3f53aeec9873bea4ca8bc1d5fc
moostofa <mmdrahinz96@gmail.com>
2021-11-05 09:45:01 +1100

    category menu on all pages

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -1,8 +1,8 @@
 from .models import Listing
 
-def category_list(request):
+def category_context(request):
     categories = list(Listing.objects.values_list("category", flat = True))
     categories = sorted({category.lower().capitalize() for category in categories if category})
     return {
-        "categories_list": categories
+        "category_menu": categories
     }
