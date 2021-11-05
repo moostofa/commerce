0762c498e4fcbe17cb75a4a40e357b0d4cab86cb
moostofa <mmdrahinz96@gmail.com>
2021-11-05 15:02:11 +1100

    linked context processor

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -1,7 +1,9 @@
 from .models import Listing
 
 def category_context(request):
+    # get a values_list of the category field for every listing item
     categories = list(Listing.objects.values_list("category", flat = True))
+    # convert into a sorted set
     categories = sorted({category for category in categories if category})
     return {
         "category_menu": categories
